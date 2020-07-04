f1eb5abb759e85cab22ce121d6066d2b2817f0f9
Stuart Larsen <c0nrad@c0nrad.io>
2020-07-04 08:59:19 -0400

    add superdense circuit

---
+++ circuits/superdense/README.md
@@ -0,0 +1,19 @@
+# Superdense Coding
+
+Transmit two bits of classical information in one qubit! (sort of, maybe)
+
+### Steps
+1. Eve generates an entangled qubit pair, one goes to alice, one goes to bob
+2. alice encodes her 2-bit message on her single qubit
+3. alice sends her qubit to bob
+4. Bob is able to decode the message
+
+### How it works
+
+Depending on the message Alice wants to send, she performs some X,Z gates on her qubit. This will modify both |0> to |1> (and vice versa), and the phase |0> to -|0>. 
+
+Using this Bob is able to decode the message.
+
+### Running
+
+go run superdense.go
---
+++ circuits/superdense/superdense.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/c0nrad/qrad"
+)
+
+// 1, 0, 2 1
+func encodeCharacter(r rune) []int {
+	out := []int{}
+
+	for i := 0; i < 4; i++ {
+		out = append(out, int(r%4))
+		r >>= 2
+	}
+	return out
+}
+
+func decodeCharacter(in []int) string {
+	sum := byte(0)
+
+	for i := 3; i >= 0; i-- {
+		sum <<= 2
+		sum += byte(in[i])
+	}
+	return string(sum)
+}
+
+func main() {
+
+	messageStr := "hello world!!"
+	message := []int{}
+	for _, c := range messageStr {
+		message = append(message, encodeCharacter(rune(c))...)
+	}
+
+	out := ""
+	buff := []int{}
+
+	for _, m := range message {
+		c := qrad.NewCircuit([]int{0, 0})
+
+		fmt.Println("Entangle two qubits")
+		c.Append(qrad.H, []int{0})
+		c.AppendControl(qrad.X, []int{0}, 1)
+		c.Draw()
+
+		fmt.Println("Alice encodes her message")
+		switch m {
+		case 0:
+			break
+		case 1:
+			c.Append(qrad.X, []int{0})
+		case 2:
+			c.Append(qrad.PauliZGate, []int{0})
+		case 3:
+			c.Append(qrad.X, []int{0})
+			c.Append(qrad.PauliZGate, []int{0})
+		}
+		c.Draw()
+
+		// Bob decoders the info
+		fmt.Println("Bob Decodes the message")
+		c.AppendControl(qrad.X, []int{0}, 1)
+		c.Append(qrad.H, []int{0})
+		c.Draw()
+		c.Execute()
+
+		if c.Measure() != m {
+			panic("Failed to encode information")
+		}
+
+		buff = append(buff, m)
+
+		if len(buff) == 4 {
+			out += decodeCharacter(buff)
+			fmt.Println(out)
+			buff = []int{}
+		}
+		time.Sleep(time.Millisecond * 100)
+	}
+
+	if out != messageStr {
+		panic("Failed to decode message")
+	}
+}
